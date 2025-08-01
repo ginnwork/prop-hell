0a8f88f76f8156785c6dde498c06d05da8e0c60b
Jonathan Ginn <jonathan@ginn.work>
2025-08-01 17:45:09 +0000

    Added data.tsx from playground.solidjs.com

---
+++ data.tsx
@@ -0,0 +1,60 @@
+export default [
+    {
+        "name": "Alpha",
+        "rows": [
+            {
+                "name": "Alpha One",
+                "cells": [
+                    { "name": "A1 Cell 1" },
+                    { "name": "A1 Cell 2" },
+                    { "name": "A1 Cell 3" }
+                ]
+            },
+            {
+                "name": "Alpha Two",
+                "cells": [
+                    { "name": "A2 Cell 1" },
+                    { "name": "A2 Cell 2" },
+                    { "name": "A2 Cell 3" }
+                ]
+            },
+            {
+                "name": "Alpha Three",
+                "cells": [
+                    { "name": "A3 Cell 1" },
+                    { "name": "A3 Cell 2" },
+                    { "name": "A3 Cell 3" }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "Bravo",
+        "rows": [
+            {
+                "name": "Bravo One",
+                "cells": [
+                    { "name": "B1 Cell 1" },
+                    { "name": "B1 Cell 2" },
+                    { "name": "B1 Cell 3" }
+                ]
+            },
+            {
+                "name": "Bravo Two",
+                "cells": [
+                    { "name": "B2 Cell 1" },
+                    { "name": "B2 Cell 2" },
+                    { "name": "B2 Cell 3" }
+                ]
+            },
+            {
+                "name": "Bravo Three",
+                "cells": [
+                    { "name": "B3 Cell 1" },
+                    { "name": "B3 Cell 2" },
+                    { "name": "B3 Cell 3" }
+                ]
+            }
+        ]
+    }
+]
